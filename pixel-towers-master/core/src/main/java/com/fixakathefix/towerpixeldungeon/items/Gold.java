0def8655eb287f4d6343c70fc8ff5c57c04c7101
Purplecorenuetral <76774804+Purplecorenuetral@users.noreply.github.com>
2026-06-05 20:39:15 +0800

    increase coin drops by approx. 1.75x from the base.

--- pixel-towers-master/core/src/main/java/com/fixakathefix/towerpixeldungeon/items/Gold.java
+++ pixel-towers-master/core/src/main/java/com/fixakathefix/towerpixeldungeon/items/Gold.java
@@ -86,7 +86,7 @@ public class Gold extends Item {
 	
 	@Override
 	public Item random() {
-		quantity = Random.IntRange( 30 + Dungeon.depth * 10, 60 + Dungeon.depth * 20 );
+		quantity = Random.IntRange( 53 + Dungeon.depth * 17, 124 + Dungeon.depth * 34 );
 		return this;
 	}
 
